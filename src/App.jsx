ab4e8e4de0b9f7de013914a71a66a8b3d71ba276
Vanderson Santos <93881616+VandersonSantosSilva@users.noreply.github.com>
2023-04-21 22:04:29 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
 import { Provider } from 'react-redux';
-import {legacy_createStore } from 'redux';
+import { createStore } from 'redux';
 import PokemonsReducers from './PokedexReducers/PokemonsReducers';
 import PokemonsList from './Components/PokemonsList';
 import Search from './Components/Search';
 import InformationsPokemons from './Components/InformationsPokemons';
-import {Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-const store =  legacy_createStore(PokemonsReducers);
+const store = createStore(PokemonsReducers);
 
 function App() {
   return (
     <Provider store={store}>
-        <Routes>
-          <Route path={`/pokemon/:id`} element={<InformationsPokemons />} />
-          <Route exact path="/"element={[<PokemonsLit />, <Search />]}/>
-        </Routes>
+      <Routes>
+        <Route path={`/pokemon/:id`} element={<InformationsPokemons />} />
+        <Route exact path="/" element={[<PokemonsList />, <Search />]} />
+      </Routes>
     </Provider>
   );
 }
